29a8fb9714755116654aeb68938478d08eb7477c
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-29 22:53:18 +0530

    Create 31.C

---
+++ 31.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   int a,b=1,c=0,e,f,i,g,h;
+   char s[100000],d[100000];
+   printf("Input :");
+   for(i=0;i<100000;i++)
+   s[i]='\0';
+   gets(s);
+   printf("Output :");
+   for(f=0;s[f]!='\0';f++);
+   for(i=0;i<f;i++)
+   {
+       if(s[i]!=' ')
+       c++;
+   }
+   printf("%d",c);
+    return 0;
+}
