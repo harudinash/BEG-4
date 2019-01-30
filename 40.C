06e706947e39b48774bfb3253bd964d77af547c5
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-30 07:35:16 +0530

    Update and rename HW_2ND.C to 40.C

---
+++ 40.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,j,n=0,c=0,e=0,t=0,h;
+printf("Input :");
+
+scanf("%d",&b);
+printf("Output :\n");
+a=1;
+//printf("%d ",a);
+t=a;
+n=0;
+for(i=a;i<=b;i++)
+ {
+  c=0;
+  e=0;
+ 
+   if(c==0)
+    {
+      printf("%d ",t);
+//     printf(" p=%d ",i);
+
+
+
+
+ t=t+n;
+ n=t-n;
+   }
+   }
+
+   getch();
+
+
+	     }
